7df7caf1092b4537ded1a9b1ec3d5e3d5c5d0a99
DeoBye <boyi.chen@tum.de>
2025-04-12 21:53:04 +0000

    restructured

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from torch import nn
 import torch.nn.functional as F
 from tqdm import tqdm
 import os
-from geoclip import GeoCLIP, GeoDataLoader, GeoDataLoader_im2gps, GeoDataLoader_osm5v
-from geoclip import img_train_transform, img_val_transform, train, eval_images
+from geoclip import GeoCLIP, GeoDataLoader, GeoDataLoader_im2gps, GeoDataLoader_osv5m
+from geoclip import img_train_transform, img_val_transform, train, train_ssl, eval_images
 from geoclip.utils.logger import *
-from geoclip import CrossEntropyLoss, TripletLoss, EntropywithDis
-
-###  before run: main中改experiment path， log名字   loss_fn      geoclip.py中改loss_fn    loss中改
+from geoclip import *
 
 
 
+###   main中改experiment path， log名字   loss_fn      geoclip.py中改loss_fn    loss中改
 def add_gps_noise(gps, std_dev=0.01):
     """Injects Gaussian noise into GPS coordinates."""
     noise = torch.randn(gps.size()) * std_dev
@@ -28,18 +27,23 @@ def save_checkpoint(model, save_dir):
     torch.save(model.logit_scale.data, os.path.join(save_dir, "logit_scale_weights.pth"))
 
 
-def get_loss(loss_name, **kwargs):
-    if loss_name == "crossentropy":
-        return CrossEntropyLoss(**kwargs)
-    if loss_name == "triplet":
-        return TripletLoss(**kwargs)
-    if loss_name == "entropywithdis":
-        return EntropywithDis(**kwargs)
+def build_strategy(strategy_name, model, device):
+    strategy_map = {
+        "clip": Clip_Inspired,
+        "ssl": SSL,
+        "triplet": Triplet,
+        "entropy": EntropywithDis,
+        "semi": SemivarioPenalty,
+        "weighted": WeightedLogits,
+        "weighted2": WeightedLogits2,
+        "weighted_neg": WeightedwithNeg
+    }
+    return strategy_map[strategy_name](model=model, device=device)
 
 # Main script for training and validating GeoCLIP
 def main(validate_only=False, test_only=False):
     # Hyperparameters
-    experiment_path = "./experiments/"                              #####
+    experiment_path = "./experiments/mp16/sslbatchnorm"                              #####
     os.makedirs(experiment_path, exist_ok=True)
     
     log_file = os.path.join(experiment_path, "logger.log")                   #####
@@ -52,51 +56,50 @@ def main(validate_only=False, test_only=False):
     
     gps_noise_std = 0.01  # Standard deviation for GPS noise
     # model_save_path = "geoclip.pth"
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")    #####
-    # device = torch.device("cpu")
+    device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")    #####
+                     ####
+    
+    #  device = torch.device("cpu")
     print(device)
 
     # Initialize model, optimizer, and scheduler
-    model = GeoCLIP(from_pretrained=True, queue_size=4096).to(device)
+    model = GeoCLIP(from_pretrained=False, queue_size=4096).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.87)
     
-    loss_fn = 'entropywithdis'                             ####      if crossentropy: geoclip
-    if loss_fn == 'crossentropy':
-        criterion = get_loss('crossentropy', model=model, device=device)
-    elif loss_fn == 'triplet':
-        criterion = get_loss('triplet', model=model, device=device)
-    elif loss_fn == "entropywithdis":
-        criterion = get_loss('entropywithdis', model=model, device=device)
+    strategy_name = "ssl"
+    strategy = build_strategy(strategy_name, model, device)
     
-    print_log(f"Loss Function: {loss_fn}", logger)
+    print_log(f"Strategy: {strategy_name}", logger)
     
     # Prepare data loaders
-    train_dataset = GeoDataLoader("/root/global-streetscapes/data/map/map_filter.csv", "/root/global-streetscapes/data/map/images", transform=img_train_transform())
-    # val_dataset = GeoDataLoader("/root/val.csv", "/root/data", transform=img_val_transform())
-    val_dataset = GeoDataLoader("/root/global-streetscapes/data/map/val.csv", "/root/global-streetscapes/data/map/images", transform=img_val_transform())
-    # test_dataset = GeoDataLoader_im2gps("/root/Im2GPS3K/im2gps3k_places365.csv", "/root/Im2GPS3K/im2gps3k_rgb_images", transform=img_val_transform())
-    test_dataset = GeoDataLoader_osm5v("/root/osm5v/filtered.csv", "/root/osm5v/00", transform=img_val_transform())
-    
+    train_dataset = GeoDataLoader("/data/roof/mp16/MP16-Pro/subset.csv", "/data/roof/mp16/MP16-Pro/subset", 'mp16', transform=img_train_transform())         #####
+    # train_dataset = GeoDataLoader_osv5m("/data/roof/osv5m/test_with_paths.csv", "/data/roof/osv5m/images/test", transform=img_train_transform())
+    val_dataset = GeoDataLoader("/data/roof/osv5m/test_test.csv", "/data/roof/osv5m/images/test", 'osv5m', transform=img_val_transform())                          #####
+    test_dataset = GeoDataLoader("/root/im2gps3k_places365.csv", "/root/im2gps3k_rgb_images", 'mp16', transform=img_val_transform())
+
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
     test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
     
     best_acc = 0.0
     
-    if validate_only:
-    # Only validate the dataset
-        print_log("Validating the model...", logger)
-        val_loss, val_accuracy = eval_images(val_dataloader, model, device)
-        print_log(f"Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_accuracy:.4f}", logger)
-        return
+    
+
+    
+    # if validate_only:                                                   #####
+    # # Only validate the dataset
+    #     print_log("Validating the model...", logger)
+    #     val_loss, val_accuracy = eval_images(val_dataloader, model, device)
+    #     print_log(f"Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_accuracy:.4f}", logger)
+    #     return
     # Training and validation loop
 
     
     if test_only:
         print_log("Test the model...", logger)
         val_loss = eval_images(test_dataloader, model, device=device, logger=logger)
-        print_log(f"Test Loss: {val_loss:.4f}, Test Accuracy: {val_accuracy:.4f}", logger)
+        print_log(f"Test Loss: {val_loss:.4f}, Test Accuracy: {val_loss:.4f}", logger)
         return
     
     print_log("starting training ...", logger)
@@ -106,11 +109,12 @@ def main(validate_only=False, test_only=False):
         epoch_dir = os.path.join(experiment_path, f"epoch_{epoch}")
         os.makedirs(epoch_dir, exist_ok=True)
         
-        train(train_dataloader, model, optimizer, epoch, batch_size, device, criterion, scheduler, gps_noise_std=gps_noise_std, logger=logger)
+        # train(train_dataloader, model, optimizer, epoch, batch_size, device, criterion, scheduler, gps_noise_std=gps_noise_std, logger=logger)
+        train(train_dataloader, model, optimizer, epoch, strategy, strategy_name, scheduler, logger=logger)
         
         # Validate after each epoch
-        print_log("validate: ...", logger)
-        val_acc = eval_images(val_dataloader, model, device=device, logger=logger)
+        print_log("validate on osv5m: ...", logger)                                    #####
+        val_acc = eval_images(val_dataloader, model, device=device, logger=logger)               #####
         print_log("test on im2gps3k: ...", logger)
         test_acc = eval_images(test_dataloader, model, device=device, logger=logger)
         # Save the model checkpoint
